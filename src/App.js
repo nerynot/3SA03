e2e5379ced17ea3b0daab0104bd20d49022a75d0
nerynot <6210110502@psu.ac.th>
2021-07-12 01:13:31 +0700

    ADD WORD

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import WordCard from './WordCard';
-import _ from 'lodash'
+import _, { random } from 'lodash'
 
 
 function App() {
-  const word = ["LEAN","SIT","HIT","CRAWL","CLIMB","CARRY","FLY","SWIM"];
-  const randomWord = _.sample(word);
+  /*const all = [word1,word2];*/
+  const word1 = ["LEAN","SIT","HIT","CRAWL","CLIMB","CARRY","FLY","SWIM"];
+  const word2 = ["PIZZA","SANDWICH","PASTA","CHIPS","CAKE","PIE"];
+  /*const randomWord = _.sample(all);*/
+  const randomWord1 = _.sample(word1);
+  const randomWord2 = _.sample(word2);
 
   const newGame = () => {
     window.location.reload()
@@ -14,14 +18,21 @@ function App() {
   return (
     <div className="head">
       <header>
-        <h1> English word activity game </h1>
+        <h1> English word game </h1>
       </header>
         <div className="maingame" >
-          <WordCard value = {randomWord}/>
+        <h1> word activity  </h1>
+          <WordCard value = {randomWord1}/>
+        <h1> word   </h1>
+          <WordCard value = {randomWord2}/>
+        
         </div>
         <button className="newgame" onClick={newGame}>
           Restart
         </button>
+        
+        
+          
         
       
     </div>
--- src/WordCard.js
+++ src/WordCard.js
@@ -28,7 +28,6 @@ export default function WordCard(props){
             if (guess == state.word){
                 console.log('yeah!')
                 setState({...state, completed : true})
-                score=true ;
             }else {  
                 console.log('reset,next attempt')
                 setState({...state,guess: '', attempt: state.attempt + 1 }) 
